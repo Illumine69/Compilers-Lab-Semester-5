0d186bd33fc1da16999f41b848078ebd71f0835c
Illumine69 <sanskarmittal2002@gmail.com>
2023-08-24 18:19:21 +0530

    cleaned files

--- Assignment_3/ass3_21CS10057_21CS30058.c
+++ Assignment_3/ass3_21CS10057_21CS30058.c
@@ -3,6 +3,7 @@ Name: Sanskar Mittal              Roll No:   21CS10057
 Name: Voddula Karthik Reddy       Roll No:   21CS30058
 Semester: 5th
 Assignment 3: Lexer for tinyC
+File: main file
 */
 
 #include <stdio.h>
--- Assignment_3/ass3_21CS10057_21CS30058_test.c
+++ Assignment_3/ass3_21CS10057_21CS30058_test.c
@@ -1,3 +1,12 @@
+/*
+Name: Sanskar Mittal              Roll No:   21CS10057
+Name: Voddula Karthik Reddy       Roll No:   21CS30058
+Semester: 5th
+Assignment 3: Lexer for tinyC
+File: test file
+*/
+
+
 #include <stdio.h>
 
 void main(){
