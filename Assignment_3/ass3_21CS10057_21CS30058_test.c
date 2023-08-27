ebc06673fd659232d2ae1136651e52ad85ddec8e
vkr2471 <vkr2471@gmail.com>
2023-08-27 21:31:46 +0530

    Update - fix merge conflicts

--- Assignment_3/ass3_21CS10057_21CS30058_test.c
+++ Assignment_3/ass3_21CS10057_21CS30058_test.c
@@ -6,24 +6,101 @@ Assignment 3: Lexer for tinyC
 File: test file
 */
 
+typedef struct{
+	int a;
+	char b[20]="This is a struct";
+	struct node* next;
+}node;
+
+
+
+
+
 #include <stdio.h>
 
-void main(){
+static int stat;
 
-    //testing all the keywords here
-	auto, break, case, char, const, continue, default, do, double, else
-	enum, extern, float, for, goto, if, inline, int, long, register
-	restrict, return, short, signed, sizeof, static, struct, switch, typedef, union
- 	unsigned, void, volatile, while, _Bool, _Complex, _Imaginary;
+volatile int vola;
 
- 	
- 	/* Multi line  comment*/
+inline int add();
 
- 	// testing identifiers and constants:
- 	float abcd = -45.56;
- 	int defg = 89;
- 	char check[10] = "/* */";
- 	char ch='a';
+void main()
+{
+	// This is a single line comment
 
-    return 0;
-}
+
+	/* 
+		This is a multiline comment
+	   This is a multiline comment
+	*/
+	int a[200];
+	int n=4,i=0;
+	unsigned int t=4;
+	short int shorty=2;
+	register r;
+	char character='c';
+	extern int yylex();
+
+
+	float abcdef =12.3345;
+	long long int c=1E17;
+	char b[200]="This is a string\n \t";
+		_Bool b=1;
+		double _Complex cmplx=1;
+	for(auto  i=0;i<n;i++)
+	{
+		i+=4;
+		if(i>3)break;
+		continue;
+	}
+		i--;
+
+
+
+	do {
+		i++;
+
+	}while(i<n||i<54&&i>32);
+	
+	int size =sizeof(int );
+	if(t>shorty){
+		shorty=t;
+	}
+	else if(shorty<t){
+		node.a=1234;
+		n=b&t;
+		n=n*4;
+		n=n~4;
+		if(n<=t);
+		if(t>=n);
+		...a;
+		n^=t;
+		n+=t;
+		n-=t;
+		n|=t;
+		n*=t;
+		n<<=t;
+		n>>=t;
+		n%=4;
+		n/=56;
+
+
+		
+
+		node->next=node;
+
+
+
+
+	}
+	else {
+			
+	}
+	if(n==t){
+
+	}
+
+
+
+
+ }
